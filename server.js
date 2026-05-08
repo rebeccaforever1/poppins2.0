619682830abf7ab495e8c34caa8a55aeb677e72d
rebeccaforever1 <rebeccaforever@ymail.com>
2026-05-07 19:21:28 -0700

    skip dotenv in production

--- server.js
+++ server.js
@@ -1,4 +1,6 @@
-require('dotenv').config({ path: '.env.local' });
+if (process.env.NODE_ENV !== 'production') {
+    require('dotenv').config({ path: '.env.local' });
+}
 const express = require('express');
 const cors = require('cors');
 const path = require('path');
